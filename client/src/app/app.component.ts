23d516a706bf8dc5fb14b26c668a41b159766515
Pedro Barreiro <pedromb.dev@gmail.com>
2025-08-15 08:45:43 -0300

    feat: end of section 10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
 
 import { ShopComponent } from "./features/shop/shop.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
